9aa81cdf954658d811d20f80b6ee7f394307e1f1
Frank Cruz <fcruz@ctdevelopers.com>
2025-01-12 01:44:13 +0000

    [Mui] 	- Implemented and use MUI components

--- src/Components/Shop.js
+++ src/Components/Shop.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Button } from '@mui/material'
 
 export default function Shop() {
   return (
     <div>
         <h1>Shop</h1>
+        <Button variant='outlined'>Outlined</Button>
     </div>
   )
 }
