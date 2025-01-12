4d3425c5fb4101677e1131fbcfdea1aca3b7e21d
Frank Cruz <fcruz@ctdevelopers.com>
2025-01-12 01:40:23 +0000

    [React Bootstrap] 	- Implemente and use React Bootstrap components.

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Button, Spinner } from 'react-bootstrap'
 
 export default function Home() {
   return (
     <div>
         <h1>Home</h1>
+        <Button ariant="warning">Warning</Button>
+        <Spinner animation="border" role="status">
+          <span className="visually-hidden">Loading...</span>
+        </Spinner>
     </div>
   )
 }
